58bc617ccdb445dadac2bbc9c2b18583de0240de
Erlend Oftedal <erlend@oftedal.no>
2023-01-26 13:07:09 +0100

    Fix typo

--- cli.js
+++ cli.js
@@ -31,7 +31,7 @@ const possibleArgs = {
   '--entrypoint <entrypoint>'     : 'Optional: Entrypoint when starting container - default: npm start',
   '--labels <labels>'             : 'Optional: Comma-separated list of key value pairs to use as labels',
   '--label <label>'               : 'Optional: Single label (name=value). This option can be used multiple times.',
-  '--envs <envs>'                 : 'Optional: Comma-separated list of key value paris to use av environment variables.',
+  '--envs <envs>'                 : 'Optional: Comma-separated list of key value pairs to use av environment variables.',
   '--env <env>'                   : 'Optional: Single environment variable (name=value). This option can be used multiple times.',
   '--setTimeStamp <timestamp>'    : 'Optional: Set a specific ISO 8601 timestamp on all entries (e.g. git commit hash). Default: 1970 in tar files, and current time on manifest/config',
   '--verbose'                     : 'Verbose logging',
@@ -73,7 +73,7 @@ let options = {
 
 keys.map(k => options[k] = program[k] || options[k]);
 
-delete options["label"];
+delete options['label'];
 delete options['env'];
 
 function splitLabelsIntoObject(labelsString) {
