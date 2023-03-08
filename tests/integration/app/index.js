345f5fb0d5145ba8692634871094d575dacdd601
Erlend Oftedal <erlend@oftedal.no>
2023-03-08 13:01:38 +0100

    Making test more readable

--- tests/integration/app/index.js
+++ tests/integration/app/index.js
@@ -1,1 +1,5 @@
+// Require and new up something to ensure we can reach the dependencies
+const Express = require("express");
+const app = new Express();
+
 console.log("I'm a demo app");
--- tests/localtest/test.sh
+++ tests/localtest/test.sh
@@ -10,23 +10,23 @@ rm -rf tmp
 mkdir -p tmp/certs
 mkdir -p tmp/auth
 
-printf "Generating key for local registry...\n"
+printf "* Generating key for local registry...\n"
 openssl req \
   -newkey rsa:4096 -nodes -sha256 -keyout tmp/certs/domain.key \
   -addext "subjectAltName = DNS:myregistry.domain.com" \
   -subj "/C=NO/ST=Doqr/L=Doqr/O=Doqr Integration/OU=Test Department/CN=doqr.test" \
-  -x509 -days 365 -out tmp/certs/domain.crt
+  -x509 -days 365 -out tmp/certs/domain.crt > /dev/null 2>&1
 
 
-printf "\nGenerating password for local registry...\n"
+printf "* Generating password for local registry...\n"
  docker run \
   --entrypoint htpasswd \
   httpd:2 -Bbn $TESTUSER $TESTPASSWORD > tmp/auth/htpasswd
 
-printf "\nStopping any running local doqr test registry...\n"
-docker stop registry-doqr-test || echo "No running container registry, so nothing to stop"
+printf "* Stopping any running local doqr test registry...\n"
+docker stop registry-doqr-test >/dev/null 2>&1 || echo "No running container registry, so nothing to stop"
 
-printf "\nStarting local doqr test registry on port 5443...\n"
+printf "* Starting local doqr test registry on port 5443...\n"
 docker run -d \
   --rm \
   --name registry-doqr-test \
@@ -39,30 +39,31 @@ docker run -d \
   -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
   -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
   -p 5443:5443 \
-  registry:2
+  registry:2 > /dev/null
 
-printf "\nPulling node:alpine as base image...\n"
-docker pull node:alpine 
 
-printf "\nPushing base image to local doqr test registry...\n"
-docker tag node:alpine localhost:5443/node
-docker push localhost:5443/node
+printf "* Pulling node:alpine as base image...\n"
+docker pull node:alpine  > /dev/null
 
-printf "\nRunning doqr to pull from and push result to the local doqr test registry...\n"
+printf "* Pushing base image to local doqr test registry...\n"
+docker tag node:alpine localhost:5443/node > /dev/null
+docker push localhost:5443/node > /dev/null
+
+printf "* Running doqr to pull from and push result to the local doqr test registry...\n"
 ../../lib/cli.js --fromImage node --registry https://localhost:5443/v2/ --toImage doqr-integration-test:localtest --folder ../integration/app --setTimeStamp "2023-03-07T12:53:10.471Z" --allowInsecureRegistries --token "Basic $BASICAUTH"
 
 
-printf "\nPulling image from registry to local docker daemon...\n"
-docker pull localhost:5443/doqr-integration-test:localtest
+printf "\n* Pulling image from registry to local docker daemon...\n"
+docker pull localhost:5443/doqr-integration-test:localtest > /dev/null
 
-printf "\nRunning image on local docker daemon...\n"
+printf "* Running image on local docker daemon...\n"
 docker run --rm -it localhost:5443/doqr-integration-test:localtest
 
-printf "\nDeleting image from registry to local docker daemon...\n"
-docker rmi localhost:5443/doqr-integration-test:localtest
+printf "\n* Deleting image from registry to local docker daemon...\n"
+docker rmi localhost:5443/doqr-integration-test:localtest > /dev/null
 
-printf "\nStopping local doqr test registry...\n"
-docker stop registry-doqr-test
-
+printf "* Stopping local doqr test registry...\n"
+docker stop registry-doqr-test > /dev/null
+rm -rf tmp
 
 printf "\nSUCCESS!\n"
