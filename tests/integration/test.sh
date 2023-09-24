22a643f3ebe84c35a11874f1a38ea7d9cdafb82b
Vegard Hagen <vegard@stonegarden.dev>
2023-09-24 16:22:17 +0200

    fix(customContent): Some layers not being set when using customContent
    Layers for WORKDIR, ENTRYPOINT and user information were not being added
    when using customContent

--- tests/integration/test.sh
+++ tests/integration/test.sh
@@ -9,6 +9,7 @@ rm -rf tmp
 mkdir -p tmp/v1/content/
 mkdir -p tmp/v2/content/
 mkdir -p tmp/v3/content/
+mkdir -p tmp/v4/content/
 mkdir -p tmp/layercache
 
 echo "Building image 1..."
@@ -23,11 +24,15 @@ echo "Building image 3..."
 
 ../../lib/cli.js --fromImage node:alpine --toImage containerify:demo-app --folder . --toTar tmp/v3.tar --customContent customContent --setTimeStamp "2023-03-07T12:53:10.471Z" --layerCacheFolder tmp/layercache > /dev/null
 
+echo "Building image 4..."
+
+../../lib/cli.js --file ./file/containerify.json --toTar tmp/v4.tar --setTimeStamp "2023-03-07T12:53:10.471Z" --layerCacheFolder tmp/layercache > /dev/null
 
 echo "Untaring content ..."
 tar -xf tmp/v1.tar -C tmp/v1/content/
 tar -xf tmp/v2.tar -C tmp/v2/content/
 tar -xf tmp/v3.tar -C tmp/v3/content/
+tar -xf tmp/v4.tar -C tmp/v4/content/
 
 jqscript='if (.config.Entrypoint == ["npm", "start"]) then true else false end'
 
@@ -56,7 +61,7 @@ if ! cmp -s tmp/v1/content/manifest.json tmp/v2/content/manifest.json; then
 fi
 
 echo "Checking that config files for 1 and 3 are not equal ..."
-if cmp -s tmp/v1/content/config.json tmp/v3/content/maniconfigfest.json; then
+if cmp -s tmp/v1/content/config.json tmp/v3/content/config.json; then
    echo "ERROR: config.jsons are the same"; 
    exit 1;
 fi
@@ -64,6 +69,18 @@ fi
 echo "Checking that manifest files for 1 and 3 are not equal ..."
 if cmp -s tmp/v1/content/manifest.json tmp/v3/content/manifest.json; then
    echo "ERROR: manifest.jsons are the same"; 
+   exit 1;
+fi
+
+echo "Checking that config files for 3 and 4 are equal ..."
+if ! cmp -s tmp/v3/content/config.json tmp/v4/content/config.json; then
+   echo "ERROR: config.jsons are not the same";
+   exit 1;
+fi
+
+echo "Checking that manifest files for 3 and 4 are equal ..."
+if ! cmp -s tmp/v3/content/manifest.json tmp/v4/content/manifest.json; then
+   echo "ERROR: manifest.jsons are not the same";
    exit 1;
 fi
 
